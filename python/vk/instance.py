c51cd1276bb99d592a5228194350a1f8dc874658
Tom Rix <trix@redhat.com>
2022-12-18 10:30:04 -0500

    Add cleaning to instance
    Signed-off-by: Tom Rix <trix@redhat.com>

--- python/vk/instance.py
+++ python/vk/instance.py
@@ -1,10 +1,24 @@
 #
 # Copyright 2022 Tom Rix trix@redhat.com
 #
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 from vk import *
 
 class vkInstance:
     def __init__(self):
+        self.cl = []
         appInfo                   = VkApplicationInfo()
         appInfo.sType             = VK_STRUCTURE_TYPE_APPLICATION_INFO
         appInfo.apiVersion        = vk_make_api_version(0,1,3,0)
@@ -12,5 +26,25 @@ class vkInstance:
         instInfo.sType            = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO
         instInfo.pApplicationInfo = appInfo
         p = new_pVkInstance()
+        self.cl.append([delete_pVkInstance, p])
         vkCreateInstance(instInfo, None, p)
         self.v = pVkInstance_value(p)
+        self.cl.append([vkDestroyInstance, self.v, None])
+
+    def __del__(self):
+        self.clean()
+
+    def clean(self):
+        if self.cl == None:
+            return
+        for c in reversed(self.cl):
+            l = len(c)
+            if l == 2:
+                c[0](c[1])
+            elif l == 3:
+                c[0](c[1], c[2])
+            elif l == 4:
+                c[0](c[1], c[2], c[3])
+            elif l == 5:
+                c[0](c[1], c[2], c[3], c[4])
+        self.cl.clear()
