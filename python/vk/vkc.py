86564810f065961515b7bc5ba1773e3cdc93d3f5
Tom Rix <trix@redhat.com>
2022-12-19 16:41:19 -0500

    And most of the initializaiton of structures to vkc
    Signed-off-by: Tom Rix <trix@redhat.com>

--- python/vk/vkc.py
+++ python/vk/vkc.py
@@ -17,5 +17,18 @@
 from vk import *
 
 class vkc:
-    def __init__(self):
+    def __init__(self, memory, sizes, shader):
         self.instance = vkInstance()
+        self.devices = vkDevices(self.instance)
+        self.dIdx, self.qIdx, self.mIdx = self.devices.pick(memory)
+        self.device = self.devices.create(self.dIdx, self.qIdx)
+        self.deviceMemory = vkMemory(self.device, self.mIdx)
+        self.deviceMemory.allocate(memory)
+        self.buffers = vkBuffer(self.device, self.deviceMemory.v, self.qIdx)
+        self.buffers.allocate(sizes)
+        self.shader = vkShader(self.device, self.buffers, shader)
+        self.command = vkCommand(self.device, self.qIdx)
+    def run(self, x, y, z):
+        self.command.begin(self.shader, x, y, z)
+        self.command.submit()
+        self.command.end()
