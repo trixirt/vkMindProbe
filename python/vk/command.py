4b47e1b1bd72726f1f06cd01d18ea44ba6aed11f
Tom Rix <trix@redhat.com>
2022-12-20 09:59:45 -0500

    Add cleanup to command
    Signed-off-by: Tom Rix <trix@redhat.com>

--- python/vk/command.py
+++ python/vk/command.py
@@ -5,19 +5,25 @@ from vk import *
 
 class vkCommand:
     def __init__(self, device, queue):
+        self.d = device
+        self.cl = []
         info = VkCommandPoolCreateInfo()
         info.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO
         info.queueFamilyIndex = queue
         p = new_pVkCommandPool()
+        self.cl.append([delete_pVkCommandPool, p])
         vkCreateCommandPool(device, info, None, p)
         v = pVkCommandPool_value(p)
+        self.cl.append([vkDestroyCommandPool, self.d, v, None])
         info = VkCommandBufferAllocateInfo()
         info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO
         info.commandPool = v
         info.level =  VK_COMMAND_BUFFER_LEVEL_PRIMARY
         info.commandBufferCount = 1
         self.p = new_pVkCommandBuffer()
+        self.cl.append([delete_pVkCommandBuffer, self.p])
         vkAllocateCommandBuffers(device, info, self.p)
+        self.cl.append([vkFreeCommandBuffers, self.d, info.commandPool, info.commandBufferCount, self.p])
         self.c = pVkCommandBuffer_value(self.p)
         p = new_pVkQueue()
         vkGetDeviceQueue(device, queue, 0, p)
@@ -32,6 +38,21 @@ class vkCommand:
         vkCmdBindDescriptorSets(self.c, VK_PIPELINE_BIND_POINT_COMPUTE, shader.pipelineLayout, 0, 1, shader.descriptorSet, 0, None)
         vkCmdDispatch(self.c, x, y, z)
         vkEndCommandBuffer(self.c);
+
+    def clean(self):
+        if self.cl == None:
+            return
+        for c in reversed(self.cl):
+            l = len(c)
+            if l == 2:
+                c[0](c[1])
+            elif l == 3:
+                c[0](c[1], c[2])
+            elif l == 4:
+                c[0](c[1], c[2], c[3])
+            elif l == 5:
+                c[0](c[1], c[2], c[3], c[4])
+        self.cl.clear()
 
     def submit(self):
         info = VkSubmitInfo()
